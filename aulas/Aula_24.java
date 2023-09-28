5d127fcedc1d2e0193c09ddfed7bd1a3f74cdbff
douglasrmachado <douglasroqmachado@gmail.com>
2023-09-28 15:32:16 -0300

    Att

---
+++ aulas/Aula_24.java
@@ -0,0 +1,13 @@
+package aulas;
+
+public class Aula_24 {
+
+    public static void main(String[] args) {
+        
+
+        for(int i = 0, j = 0; i < 100 || j < 50; i++){
+            
+            System.out.println(i);
+        }
+    }
+}
