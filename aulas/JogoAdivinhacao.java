a0d684f4ce72376574ed2ffdf2672341c4b7c4a2
Douglas <douglasroqmachado@gmail.com>
2024-02-23 16:27:55 -0300

    Clean code aplicado

--- aulas/JogoAdivinhacao.java
+++ aulas/JogoAdivinhacao.java
@@ -3,36 +3,33 @@ package aulas;
 import java.util.Scanner;
 import java.util.Random;
 
-public class Aula_25 {
+public class JogoAdivinhacao {
     
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
         Random random = new Random();
 
-        boolean acertou = false;
         int tentativas = 10;
-        int numeroSecreto = random.nextInt();
-        long chute = 0;
+        int numeroSecreto = random.nextInt(100);
+        long chute;
 
         System.out.println(numeroSecreto);
-        while(tentativas > 0 && acertou == false) {
+
+        while(tentativas > 0) {
             System.out.println("Qual seu chute?");
             chute = input.nextLong();
 
             if(numeroSecreto == chute) {
                 System.out.println("Você acertou o número!");
-                acertou = true;
-            } else if(chute < numeroSecreto){
+                break;
+            } else {
                 --tentativas;
-                System.out.println("Número muito pequeno! " + tentativas + " tentativas restantes!");
-            }else {
-                --tentativas;
-                System.out.println("Número muito alto!" + tentativas + " tentativas restantes!");
+                System.out.println("Número " + (chute < numeroSecreto ? "muito pequeno!" : "muito alto!") + " " + tentativas + " tentativas restantes! ");
             }
         }
-
-
+        
+        input.close();
 
     }
 }
